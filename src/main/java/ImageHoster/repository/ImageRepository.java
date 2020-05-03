f44071b1f5ce048cbc9e26e77ffbcd031c390f0e
Soumya Rout <soumyarout80@gmail.com>
2020-05-03 16:14:59 +0530

    Added a feature to get image from db by title

--- src/main/java/ImageHoster/repository/ImageRepository.java
+++ src/main/java/ImageHoster/repository/ImageRepository.java
@@ -50,10 +50,11 @@ public class ImageRepository {
     //Executes JPQL query to fetch the image from the database with corresponding title
     //Returns the image in case the image is found in the database
     //Returns null if no image is found in the database
-    public Image getImageByTitle(String title) {
+    public Image getImageByTitle(Integer imageId, String title) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title and i.id = :id", Image.class)
+                    .setParameter("title", title).setParameter("id", imageId);
             return typedQuery.getSingleResult();
         } catch (NoResultException nre) {
             return null;
