89798fc08598e35b5226fc9062e4e2ad84fbe571
Soumya Rout <soumyarout80@gmail.com>
2020-05-03 16:16:52 +0530

    Modified the getImageByTitle method by passing argument titel with image id

--- src/main/java/ImageHoster/service/ImageService.java
+++ src/main/java/ImageHoster/service/ImageService.java
@@ -5,8 +5,6 @@ import ImageHoster.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -27,8 +25,8 @@ public class ImageService {
 
 
     //The method calls the getImageByTitle() method in the Repository and passes the title of the image to be fetched
-    public Image getImageByTitle(String title) {
-        return imageRepository.getImageByTitle(title);
+    public Image getImageByTitle(Integer imageId, String title) {
+        return imageRepository.getImageByTitle(imageId, title);
     }
 
     //The method calls the getImage() method in the Repository and passes the id of the image to be fetched
